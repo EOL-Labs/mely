845e29547b4eb5cf7db6bc5b94b1e84e05b4ee96
Stephen Pugliese <stephen.pugliese@outlook.com>
2014-03-26 17:11:34 -0400

    edits to example files

--- config/database.example.js
+++ config/database.example.js
@@ -1,7 +1,7 @@
 exports.config = {
 	type: "mysql",
 	hostname: "hostname",
-	port: "3306",
+	port: 3306,
 	db: "databasename",
 	username: "username",
 	password: "password"
--- config/server.example.js
+++ config/server.example.js
@@ -1,7 +1,9 @@
 exports.config = {
 	hostname: "hostname",
-	port: "80",
+	port: 80,
 	https: false,
 	options: {
+		cors: true //defauls to false,
+		location: "http://www.example.com" //change this to current hosting address
 	}
 }
