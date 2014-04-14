fe2c75378e66d0dfa4ff121f6a2adba8415d04fc
Stephen Pugliese <stephen.pugliese@outlook.com>
2014-04-14 15:23:52 -0400

    Closes #7. Builds file theming.

---
+++ static/js/admin.main.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+	$("a.files").click(function(){
+		var file = encodeURIComponent($(this).attr("id"));
+		$.ajax({
+			url: "/admin/file/" + file.toLowerCase(),
+			type: "GET",
+			success: function(data){
+				$("#filecontent").val(data[0]);
+				$("#file").val(data[1]);
+			}
+		});
+	})
+});
