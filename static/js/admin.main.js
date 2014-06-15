cc35116faaaaae817b96d50f76f6a3124b843468
Stephen Pugliese <stephen.pugliese@outlook.com>
2014-06-15 15:56:28 -0400

    Closes #38

--- static/js/admin.main.js
+++ static/js/admin.main.js
@@ -6,7 +6,22 @@ $(document).ready(function(){
 			type: "GET",
 			success: function(data){
 				$("#filecontent").val(data[0]);
-				$("#file").val(data[1]);
+				$("#filename").val(data[1]);
+			}
+		});
+	});
+	$("a#save").click(function(){
+		var filename = $("#filename").val();
+		var content = $("#filecontent").val();
+		$.ajax({
+			url: "/admin/file/save",
+			type: "POST",
+			data:{
+				filename: filename,
+				content: content
+			},
+			success: function(data){
+				console.log(data);
 			}
 		});
 	});
